999d0c61ab4a0fa73c1c36cc4144c5ab74145e9c
Marc-Marquez-93 <miguel.duran.14.17@outlook.com>
2025-12-06 16:48:59 -0500

    get lectura email

--- controllers/lectura.js
+++ controllers/lectura.js
@@ -2,7 +2,9 @@ import Lectura from "../models/lecturas.js"
 
 const getLectura= async (req,res)=>{
     try {
-        const lecturas= await Lectura.find()
+        const {email} = req.params;
+
+        const lecturas= await Lectura.find({ usuario_email: email })
         res.json({lecturas})
     } catch (error) {
         res.status(400).json({error})
--- routes/lectura.js
+++ routes/lectura.js
@@ -5,18 +5,21 @@ import { check } from "express-validator";
 
 const router = Router();
 
-router.post('/principal/:usuario_id', 
-    // [a,
-    // validarCampos],
- getLectura);
+// router.post('/principal/:usuario_id', 
+//     // [a,
+//     // validarCampos],
+// );
+
 // router.post('/diaria/:usuario_id', 
 //     // [a,
 //     // validarCampos],
 //  );
-// router.get('/usuario/:usuario_id', 
-//     // [a,
-//     // validarCampos],
-// );
+
+router.get('/usuario/:usuario_id', 
+    // [a,
+    // validarCampos],
+getLectura);
+
 // router.get('/:id',
 //     // [a,
 //     // validarCampos],
